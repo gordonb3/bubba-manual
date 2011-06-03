aa2418d22412db24bd18e597fcb9a4394567391e
Carl Fürstenberg <carl@excito.com>
2011-06-03 12:13:19 +0200

    Imported Debian version 2.1.0

---
+++ manual/index.php
@@ -0,0 +1,18 @@
+<?php
+$lang = $_GET['lang'];
+if( ! $lang ) {
+	$lang = 'en';
+}
+
+$file = "$lang/Getting_Started.pdf"; 
+if( file_exists( $file ) ) {
+	header('Content-Type: ' . mime_content_type($file));
+	header('Content-Disposition: inline; filename="Getting Started.pdf"');
+	header('Pragma: public');
+	header('Content-Length: ' . filesize($file));
+	header('Content-Transfer-Encoding: binary');
+	ob_clean();
+	flush();
+	@readfile($file);
+}
+exit;
